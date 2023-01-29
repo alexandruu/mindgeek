7422b3bc42a1a66c848f1d9a77d2f9fbed874ba5
Alexandru Ungureanu <alexandru.ungureanu.986@gmail.com>
2023-01-29 19:45:28 +0200

    Fix FileCacheTest

--- tests/Unit/FileCacheTest.php
+++ tests/Unit/FileCacheTest.php
@@ -4,14 +4,17 @@ namespace Tests\Unit\Services;
 
 use App\Exceptions\FileIsNotAccessibleCacheException;
 use App\Interfaces\StorageInterface;
-use App\Services\FileCache;
-use App\Services\Storage;
+use App\Services\Caches\FileCache;
+use App\Services\Storage\StorageService;
 use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class FileCacheTest extends TestCase
 {
-    protected StorageInterface $storage;
+    public const CACHE_CONTENT_BASE64_ENCODED = 'dGVzdCBjb250ZW50';
+    public const CACHE_CONTENT_BASE64_DECODED = 'test content';
+
+    protected StorageInterface $storageService;
     protected FileCache $service;
     protected $result;
 
@@ -19,31 +22,48 @@ class FileCacheTest extends TestCase
     {
         parent::setUp();
 
-        $this->storage = \Mockery::mock(Storage::class);
+        $this->storageService = \Mockery::mock(StorageService::class);
         $this->service = $this->getMockOfFileCache(['generateFileName']);
     }
 
     public function testSaveFileInCache()
     {
-        $this->service->shouldReceive('generateFileName')
-            ->once();
-
-        $this->service->saveFileInCache(null, "test.jpg");
+        $this->setExpectationsForCacheAndServiceToSaveAFileInCacheWithSuccess();
+        $this->requestToCacheAFile();
     }
 
-    // public function testExceptionIsThrownWhenFileIsNotSavedInCache()
-    // {
-    //     $this->setExceptionExpectation();
-    //     $this->prepareFileCacheToThrowExceptionWhenTringToSaveFileInCache();
-    //     $this->requestAFile();
-    // }
+    public function testExceptionIsThrownWhenFileIsNotSavedInCache()
+    {
+        $this->setExceptionExpectation();
+        $this->prepareFileCacheToThrowExceptionWhenTringToSaveFileInCache();
+        $this->requestToCacheAFile();
+    }
 
-    // public function testEntireFlowWhenRequestingAFile()
-    // {
-    //     $this->prepareStorageAndFileCache();
-    //     $this->requestAFile();
-    //     $this->checkIfUrlForFileIsReturned();
-    // }
+    public function testGetFileContentFromCache()
+    {
+        $this->setExpectationsForCacheToGetFileContentFromCacheWithSuccess();
+        $this->requestFileContentFromCache();
+        $this->checkIfFileContentIsOk();
+    }
+
+    private function setExpectationsForCacheAndServiceToSaveAFileInCacheWithSuccess()
+    {
+        $this->service->shouldReceive('generateFileName')
+            ->once()
+            ->andReturn('test');
+
+        Cache::shouldReceive('has')
+            ->once()
+            ->andReturn(false);
+
+        Cache::shouldReceive('rememberForever')
+            ->once();
+    }
+
+    private function requestToCacheAFile()
+    {
+        $this->service->saveFileInCache(null, "test.jpg");
+    }
 
     private function setExceptionExpectation()
     {
@@ -53,46 +73,46 @@ class FileCacheTest extends TestCase
     private function prepareFileCacheToThrowExceptionWhenTringToSaveFileInCache()
     {
         $this->service->shouldReceive('generateFileName')
-            ->once()
-            ->shouldReceive('saveFileInCache')
-            ->once()
-            ->andThrow(new FileIsNotAccessibleCacheException())
-            ->shouldNotReceive('saveFileFromCacheToDisk');
-    }
-
-    private function requestAFile()
-    {
-        $this->result = $this->service->get('test');
-    }
-
-    private function prepareStorageAndFileCache()
-    {
-        $this->storage->shouldReceive('url')
-            ->once()
-            ->andReturn('test.jpg');
-
-        $this->service->shouldReceive('generateFileName')
-            ->once()
-            ->shouldReceive('saveFileInCache')
-            ->once()
-            ->andReturn(true)
-            ->shouldReceive('saveFileFromCacheToDisk')
             ->once();
+
+        Cache::shouldReceive('has')
+            ->once()
+            ->andReturn(false);
+
+        Cache::shouldReceive('rememberForever')
+            ->once()
+            ->andThrow(new \Exception());
     }
 
-    private function checkIfUrlForFileIsReturned()
+    private function setExpectationsForCacheToGetFileContentFromCacheWithSuccess()
     {
-        $this->assertEquals('test.jpg', $this->result);
+        Cache::shouldReceive('has')
+            ->once()
+            ->andReturn(true);
+
+        Cache::shouldReceive('get')
+            ->once()
+            ->andReturn(self::CACHE_CONTENT_BASE64_ENCODED);
+    }
+
+    private function requestFileContentFromCache()
+    {
+        $this->result = $this->service->getFileContentFromCache('test');
+    }
+
+    private function checkIfFileContentIsOk()
+    {
+        $this->assertEquals(self::CACHE_CONTENT_BASE64_DECODED, $this->result);
     }
 
     private function getMockOfFileCache(array $methodsToMock = [])
     {
         $className = sprintf(
-            'App\Services\FileCache[%s]',
+            'App\Services\Caches\FileCache[%s]',
             implode(',', $methodsToMock)
         );
 
-        return \Mockery::mock($className, [$this->storage])
+        return \Mockery::mock($className, [$this->storageService])
             ->makePartial()
             ->shouldAllowMockingProtectedMethods();
     }
