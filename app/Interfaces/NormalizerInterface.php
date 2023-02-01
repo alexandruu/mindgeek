ba04fb55744592c78ddcf1f4eb6a81ab9e38a112
Alexandru Ungureanu <alexandru.ungureanu.986@gmail.com>
2023-02-01 21:25:03 +0200

    Fix missing method in interface

--- app/Interfaces/NormalizerInterface.php
+++ app/Interfaces/NormalizerInterface.php
@@ -2,7 +2,11 @@
 
 namespace App\Interfaces;
 
+use App\Dtos\ProviderDto;
+
 interface NormalizerInterface
 {
+    public function supportsNormalization(ProviderDto $providerDto);
+
     public function normalize($line): array|bool;
 }
